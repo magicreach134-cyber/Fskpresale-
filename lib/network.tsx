c656eefd398b82ee8ea09135c5fe60df87c4476b
magicreach134-cyber <magicreach134@gmail.com>
2026-01-13 15:25:41 +0100

    Create network.tsx

---
+++ lib/network.tsx
@@ -0,0 +1,14 @@
+// lib/network.ts
+
+export const BNB_TESTNET = {
+  chainId: 97,
+  hexChainId: "0x61",
+  chainName: "BNB Smart Chain Testnet",
+  nativeCurrency: {
+    name: "BNB",
+    symbol: "BNB",
+    decimals: 18,
+  },
+  rpcUrls: ["https://data-seed-prebsc-1-s1.bnbchain.org:8545"],
+  blockExplorerUrls: ["https://testnet.bscscan.com"],
+};
